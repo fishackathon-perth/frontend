e04cba783636a7a9e4e1dad588e4483665a8cc2e
Robert Fernandez <fernandez.robert.97@gmail.com>
2018-02-10 14:57:24 +0800

    Added Map component

--- src/component/map.jsx
+++ src/component/map.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
+
 
 export default class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: ''
-    }
-  }
-
-  render() {
-
-    return (
-    <div><h1>this is a map</h1>
-    </div>
-    )
-  }
+	render() {
+		return (
+			<MyMapComponent
+			  isMarkerShown
+			  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+			  loadingElement={<div style={{ height: `100%` }} />}
+			  containerElement={<div style={{ height: `400px` }} />}
+			  mapElement={<div style={{ height: `100%` }} />}
+			/>
+		);
+	}
 }
